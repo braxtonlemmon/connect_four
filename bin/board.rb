8c4b1a29b562fe2f75914a38b94bcf6072e4a6e4
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-05-16 10:10:01 +0200

    Create tests for game.rb

--- bin/board.rb
+++ bin/board.rb
@@ -6,12 +6,23 @@ class Board
 	end
 
 	def show
-		grid.each { |row| p row }
+		(0..6).each { |x| print "|#{x}|"}
+		puts
+		grid.map do |row|
+			line = []
+			(0..6).each { |column| line << row[column] }
+			puts "[" + line.join("][") + "]"
+		end
 	end
 
-	def drop_piece(column)
-		row = find_empty_spot(column)
-		row ? (@grid[row][column] = "x") : false
+	def drop_piece(column, piece)
+		# row = find_empty_spot(column)
+		# row ? (@grid[row][column] = piece) : false
+		if row = find_empty_spot(column)
+			@grid[row][column] = piece
+			return [row, column]
+		end
+		false
 	end
 
 	def spot_empty?(row,column)
@@ -20,6 +31,7 @@ class Board
 
 	def find_empty_spot(col)
 		5.downto(0) { |row| return row if spot_empty?(row,col) }
+		puts "Invalid move! Try again"
 		false
 	end
 
@@ -63,10 +75,16 @@ class Board
 		false
 	end
 
-	# The row and column in #match?(row,column) refer to the coordinates of the most recently dropped piece
-	def match?(row, column)
-		return true if column_win?(column) || row_win?(row) || diagonal_win?(0) || diagonal_win?(1)
+	# The row and column in #match?(row,column) refer to the coordinates 
+	# of the most recently dropped piece
+	def match?(location)
+		return true if column_win?(location[1]) || row_win?(location[0]) || diagonal_win?(0) || diagonal_win?(1)
 		false
+	end
+
+	def full?
+		return false if (0..5).any? { |row| (0..6).any? { |col| grid[row][col] == " " } }
+		true
 	end
 
 end
---
+++ bin/game.rb
@@ -0,0 +1,61 @@
+require_relative "board"
+require_relative "player"
+require_relative "cell"
+
+class Game
+	attr_reader :player1, :player2, :board, :current_player, :location
+
+	def initialize
+		@player1 = Player.new
+		@player2 = Player.new
+		@board = Board.new
+		@current_player = @player1
+		@location = Array.new(2)
+		puts welcome
+	end
+
+	def welcome
+		<<~HEREDOC
+				Welcome to Connect Four!
+				
+				Players each alternate dropping a token of their color (black or white) into
+				an unfilled column of their choosing. The game is over once a player successfully places
+				four consecutive tokens in a row horizontally, vertically, or diagonally. If the board is
+				full and neither player has achieved four in a row, the game is a draw.
+				
+				Enjoy!
+			HEREDOC
+	end
+
+	def swap_players
+		@current_player = ((@current_player == @player1) ? @player2 : @player1 )
+	end
+
+	def make_move
+		puts "#{current_player.name}, please select a column:"
+		@location = board.drop_piece(gets.chomp.to_i, current_player.piece)
+	end
+
+	def take_turn
+		while true
+			board.show
+			while true do break if make_move end
+			return win_game_over if board.match?(location)
+			return tie_game_over if board.full?
+			swap_players
+		end
+	end
+
+	def win_game_over
+		board.show
+		puts "Game over. #{current_player.name} wins!"
+	end
+
+	def tie_game_over
+		board.show
+		puts "It's a draw!"
+	end
+end
+
+game = Game.new
+game.take_turn
